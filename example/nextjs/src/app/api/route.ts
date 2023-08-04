67e8f67477f52c5dceffb01ba8e3cd4d1c0259f5
Nick Grealy ⚡️ <nickgrealy@gmail.com>
2023-08-04 20:33:44 +1000

    Fixed endpoint.

--- example/nextjs/src/app/api/route.ts
+++ example/nextjs/src/app/api/route.ts
@@ -3,9 +3,17 @@ import { NextResponse } from "next/server";
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function GET(req: Request, res: any) {
-    await sleep(500);
-    // res.setHeader("Cache-Control", "private, max-age=10, stale-while-revalidate=10");
-    // don't let object change too often - otherwise etag will change
-    // res.json({ minuteOfHour: new Date().getMinutes() });
-    return NextResponse.json({ text: 'Hello' }, { status: 200, headers: {"Cache-Control": "private, max-age=10, stale-while-revalidate=10"}})
+  await sleep(500);
+  // res.setHeader("Cache-Control", "private, max-age=10, stale-while-revalidate=10");
+  // don't let object change too often - otherwise etag will change
+  // res.json({ minuteOfHour: new Date().getMinutes() });
+  return NextResponse.json(
+    { minuteOfHour: new Date().getMinutes() },
+    {
+      status: 200,
+      headers: {
+        "Cache-Control": "private, max-age=10, stale-while-revalidate=10",
+      },
+    }
+  );
 }
